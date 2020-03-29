e67796a164cef81650bf734314cbb4265c03971f
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-03-29 15:31:32 +0300

    start project tour

---
+++ src/main/java/com/turchyn/Main.java
@@ -0,0 +1,5 @@
+package com.turchyn;
+
+public class Main {
+
+}
---
+++ src/main/java/com/turchyn/tours/TourBase.java
@@ -0,0 +1,11 @@
+package com.turchyn.tours;
+
+public class TourBase {
+    protected String tourTile;
+    //protected String tourType;
+    protected String tourLocation;
+    protected String tourTransport;
+    protected String tourNutrition;
+    protected int tourDuration; // number of days
+    protected int tourPrice;
+}
