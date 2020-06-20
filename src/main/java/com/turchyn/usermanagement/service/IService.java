9c6bee9b880aeac2ef270700171bb1d78b4132a1
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-06-20 15:58:52 +0300

    add service for TourBase and changed signature of some iterface methods

--- src/main/java/com/turchyn/usermanagement/service/IService.java
+++ src/main/java/com/turchyn/usermanagement/service/IService.java
@@ -1,16 +1,19 @@
 package com.turchyn.usermanagement.service;
 
+import com.turchyn.usermanagement.model.TourBase;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IService<T> {
-    void addData(T t);
+    void addData(T t) throws SQLException;
 
-    List getData(int id);
+    List getData(int id) throws SQLException;
 
-    void deleteData(int id);
+    void deleteData(T t) throws SQLException;
 
-    void updateData(T t, int id);
+    void updateData(T t) throws SQLException;
 
-    T getUserDataById(int id);
+    T getUserDataById(int id) throws SQLException;
 
 }
---
+++ src/main/java/com/turchyn/usermanagement/service/TourService.java
@@ -0,0 +1,35 @@
+package com.turchyn.usermanagement.service;
+
+import com.turchyn.usermanagement.dao.TourDAO;
+import com.turchyn.usermanagement.model.TourBase;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TourService implements IService<TourBase> {
+    TourDAO tourDAO = new TourDAO();
+    @Override
+    public void addData(TourBase tourBase) throws SQLException {
+        tourDAO.create(tourBase);
+    }
+
+    @Override
+    public List getData(int id) throws SQLException {
+        return  tourDAO.read();
+    }
+
+    @Override
+    public void deleteData(TourBase tourBase) throws SQLException {
+        tourDAO.delete(tourBase);
+    }
+
+    @Override
+    public void updateData(TourBase tourBase) throws SQLException {
+        tourDAO.update(tourBase);
+    }
+
+    @Override
+    public TourBase getUserDataById(int id) throws SQLException {
+        return tourDAO.getById(id);
+    }
+}
